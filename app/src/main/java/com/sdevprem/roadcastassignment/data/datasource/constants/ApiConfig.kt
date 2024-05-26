2a66bccdd711fe953b265e74ca4850df6c08c838
Prem Thakur <sdevpremthakur@gmail.com>
2024-05-26 11:45:30 +0530

    hide api key

--- app/src/main/java/com/sdevprem/roadcastassignment/data/datasource/constants/ApiConfig.kt
+++ app/src/main/java/com/sdevprem/roadcastassignment/data/datasource/constants/ApiConfig.kt
@@ -1,8 +1,10 @@
 package com.sdevprem.roadcastassignment.data.datasource.constants
 
+import com.sdevprem.roadcastassignment.BuildConfig
+
 object ApiConfig {
     const val BASE_URL = "https://api.themoviedb.org/3/"
-    const val API_KEY = "8c5742af61094b1940a55e040e112457"
+    const val API_KEY = BuildConfig.API_KEY
     const val AUTH_QUERY_KEY = "api_key"
     const val IMAGE_BASE_URL = "https://image.tmdb.org/t/p/original"
 }
